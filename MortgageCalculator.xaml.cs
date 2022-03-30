75992448a9e30c2f19ee61de71adbf7f1a3a9089
000951824 <behzad.pourdarab@student.tafesa.edu.au>
2022-03-31 08:51:44 +1030

    Developed the Mortgage Calculator Page.

--- MortgageCalculator.xaml.cs
+++ MortgageCalculator.xaml.cs
@@ -26,5 +26,60 @@ namespace Calculator
 		{
 			this.InitializeComponent();
 		}
+
+		// START --- Loan Calculation function
+		private void calculateTheLoan() {
+
+			//double principaleAmount = 0, yearsOfTheLoan = 0, yearlyInterestRate = 0;
+
+			// Vars
+			double principalLoanAmount = Convert.ToDouble(principalBarrowBox.Text);
+			double loanDurationInYears = Convert.ToDouble(yearOfLoanBox.Text);
+			double andMonthInLoanDuration = Convert.ToDouble(andMonthOfTheLoanBox.Text);
+			double yearlyInterestRate = Convert.ToDouble(yearlyInterestRateBox.Text);
+
+			double monthlyInterstRate = (yearlyInterestRate + andMonthInLoanDuration) / 1200;
+			double loanPeriodInMonth = (loanDurationInYears + andMonthInLoanDuration) * 12;
+
+			double monthlyRepayment = 0;
+
+			//monthlyRepayment = principaleAmount * ((r * (Math.Pow(1 + r, n))) / (Math.Pow(1 + r, n) - 1));
+			try
+			{
+				if (principalLoanAmount > 0 && yearlyInterestRate > 0)
+				{
+					monthlyRepayment =
+						principalLoanAmount * ((monthlyInterstRate * Math.Pow((1 + monthlyInterstRate), loanPeriodInMonth)) / (Math.Pow(1 + monthlyInterstRate, loanPeriodInMonth) - 1));
+				}
+				else
+				{
+					monthlyRepayment = 0;
+				}
+			}
+			catch (Exception ex) { throw ex; }
+
+			// (yearlyInterestRate / 1200)
+			monthlyInterestRateBox.Text = monthlyInterstRate.ToString("#.0000");
+			monthlyRepaymentBox.Text = monthlyRepayment.ToString("#.00");
+		}
+		// END --- Loan Calculation function
+
+
+		// START -------------- Calculate Button Click -------------
+		private void calculateBtn_Click(object sender, RoutedEventArgs e)
+		{
+			calculateTheLoan();
+		}
+		// END -------------- Calculate Button Click -------------
+
+
+		// START -------------- Exit Button Click -------------
+		private void exitBtn_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+		// END -------------- Exit Button Click -------------
+
 	}
+	// MortgageCalculator
 }
